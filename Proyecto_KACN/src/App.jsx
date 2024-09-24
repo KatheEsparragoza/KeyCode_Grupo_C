b40f8483a57bc4659349712c5a648aefc15a4cba
Natalia <nataliaa95ss@gmail.com>
2024-09-24 17:55:04 -0500

    push

--- Proyecto_KACN/src/App.jsx
+++ Proyecto_KACN/src/App.jsx
@@ -22,6 +22,8 @@ function App() {
         <Route path="/home" element={<Home />} />  {/* Ruta para la página de inicio */}
         <Route path="/KACN" element={<Kacn />} />  {/* Ruta para la página KACN */}
           <Route path="/services/shampoos" element={<ProductGallery />} /> {/* Ruta para la galería de Champús */}
+          <Route path="/services/conditioner" element={<Rtacondiacionador />} />  
+
         </Routes>
     </BrowserRouter>
     </>
--- Proyecto_KACN/src/pages/Rutaacondicionado.jsx
+++ Proyecto_KACN/src/pages/Rutaacondicionado.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useRef, useEffect } from 'react';
-import Navbar from '../componentes/Navbar';
+
 import PaginaAcondicionador  from "../componentes/Acondicionador";
 
 
 const Rtacondiacionador= () => {
     return(
      
-              <Navbar/>
+       <div></div>
     
     )
 };
