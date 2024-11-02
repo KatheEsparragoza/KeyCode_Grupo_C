45038f89c5e36bfa43cf4055f3fefaebbac72180
Angelarose-19 <angelitarosero774@gmail.com>
2024-11-01 19:19:48 -0500

    a

--- Proyecto_KACN/src/componentes/Menu.jsx
+++ Proyecto_KACN/src/componentes/Menu.jsx
@@ -1,27 +1,26 @@
-// import { NavLink } from "react-router-dom";
-
-// const Menu = () => {
-//   return (
+import { NavLink } from "react-router-dom";
+import { NavLink } from "";
+const Menu = () => {
+  return (
     
-//     <nav className="menu">      
-//       <NavLink
-//         className={({ isActive }) => (isActive ? "active-link" : null)}
-//         to={"/"}
-//       >
-//         Home
-//       </NavLink>
-//       <NavLink
-//         className={({ isActive }) => (isActive ? "active-link" : null)}
-//         to={"/contactos"}
-//       >
-//         Contactos
-//       </NavLink>
-//       <NavLink to="/Kacn">    
-//        <button class= "bg-red-100">Ir a Kacn</button>
-//       </NavLink>
+    <nav className="menu">        
+      <NavLink
+        className={({ isActive }) => (isActive ? "active-link" : null)}
+        to={"/"}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "active-link" : null)}
+        to={"/contactos"}
+      >
+        Contactos
+      </NavLink>
+      <NavLink to="/Kacn">    
+       <button class="bg-amber-100"> Ir a Kacn</button>
+      </NavLink>
+    </nav>
+  );
+};
 
-//     </nav>
-//   );
-// };
-
-// export default Menu;
+export default Menu;
